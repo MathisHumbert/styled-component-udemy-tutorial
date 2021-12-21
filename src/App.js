73fdd777562a27d69fde5f617a5fa9ef165c59d8
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-21 19:38:39 +0100

    react styled component tuto is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
-import Form from './components/Form';
+import Products from './components/Products';
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <Form />
+      <Products />
     </div>
   );
 }
